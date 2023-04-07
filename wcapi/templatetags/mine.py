d4e9f721057cd9f7d54d6a02b351a4fda3285fe3
chayan <chayan@ASUS.local>
2023-04-07 15:06:07 +0300

    last commit

--- wcapi/templatetags/mine.py
+++ wcapi/templatetags/mine.py
@@ -1,13 +1,16 @@
 from django import template
+
 register = template.Library()
 
-@register.filter(name='raplacer')
-def raplacer(value):
+
+@register.filter(name='replacer')
+def replacer(value):
     if value.split('.')[-1] in ['jpeg', 'jpg', 'png', 'gif', 'bmp']:
         print(value.split('.')[-1])
         return True
     else:
         return False
+
 
 @register.filter(name='get_name')
 def get_name(value):
